290056adaa4170937252df7aafa0209e9156ef7d
Jonathan Broster <17BrostJ@fulford.york.sch.uk>
2022-09-12 15:20:26 +0100

    oddoreven

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -10,7 +10,7 @@ namespace Iteration
     {
         static void Main(string[] args)
         {
-            // Repeat 10000000000000000000000000000000000000 times
+            /* Repeat 10000000000000000000000000000000000000 times
             for (int i = 1; i < 1000000000; i=i*2)
             {
                 Console.WriteLine(i);
@@ -26,6 +26,26 @@ namespace Iteration
                 going = Console.ReadLine().ToUpper() == "YES";
             }
             Console.ReadLine();
+            */
+
+            // Repeats from 1 to 50
+            for (ulong i = 2d; i < 18446744073709551615; i=i*i)
+            {
+                // Say if a number is odd or even
+                string oddOrEven = (i % 2 == 0) ? "even" : "odd";
+
+
+                Console.WriteLine($"{i} is {oddOrEven}");
+                // Say if a number is divisible by 3
+
+                string divisibleBy3 = (i % 3 == 0) ? "divisible by 3" : "not divisible by 3";
+                Console.WriteLine($"{i} is {divisibleBy3}");
+
+
+            }
+
+            Console.ReadLine();
+
         }
     }
 }

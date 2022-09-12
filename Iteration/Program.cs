dcd6da6b864a4e72e36702d69c94c54b27f0257d
Jonathan Broster <17BrostJ@fulford.york.sch.uk>
2022-09-12 14:56:22 +0100

    For and While Loops

--- Iteration/Program.cs
+++ Iteration/Program.cs
@@ -10,6 +10,22 @@ namespace Iteration
     {
         static void Main(string[] args)
         {
+            // Repeat 10000000000000000000000000000000000000 times
+            for (int i = 1; i < 1000000000; i=i*2)
+            {
+                Console.WriteLine(i);
+            }
+
+
+            // Repeat until the use says "Quit"
+            bool going = true;   
+            while(going == true)
+            {
+                Console.WriteLine("Still Going?");
+
+                going = Console.ReadLine().ToUpper() == "YES";
+            }
+            Console.ReadLine();
         }
     }
 }
